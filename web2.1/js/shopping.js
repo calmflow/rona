268209c0a727ba2c6d3cf7c27a174bdaad472766
calmflow <o.newwday@gmail.com>
2024-04-15 10:35:30 +0900

    modify: service-list animation

--- web2.1/js/shopping.js
+++ web2.1/js/shopping.js
@@ -1,42 +1,23 @@
 $(function () {
     let clickNum = 0;
 
-    // $('.btn-arrow--next').click(function(){
+    //append,prepend를 이용한 슬라이드
+
+    // $('.btn-arrow--next').click(function () {
     //     clickNum++;
 
-    //     if(clickNum > 3){ clickNum = 1;}
+    //     if (clickNum > 2) {
+    //         clickNum = 1;
+    //     }
 
-    //     let thisList = $('.service-list').eq(clickNum);
-    //     let prevList = $('.service-list').eq(clickNum-1);
+    //     console.log(clickNum);
 
-    //     $('.service-list-wrap').append(prevList);
-    // })
+    //     let firstList = $('.service-list').eq(0);
+    //     let secondList = $('.service-list').eq(1);
 
-    $('.btn-arrow--next').click(function () {
-        clickNum++;
+    //     secondList.addClass('right-in');
+    //     firstList.removeClass('right-in');
 
-        if (clickNum > 2) {
-            clickNum = 1;
-        }
-
-        console.log(clickNum);
-
-        let firstList = $('.service-list').eq(0);
-
-        $('.service-list-wrap').append(firstList);
-    });
-
-    $('.btn-arrow--prev').click(function () {
-        clickNum--;
-
-        if (clickNum < 0) {
-            clickNum = 2;
-        }
-
-        console.log(clickNum);
-
-        let lastList = $('.service-list').eq(2);
-
-        $('.service-list-wrap').prepend(lastList);
-    });
+    //     $('.service-list-wrap').append(firstList);
+    // });
 });
