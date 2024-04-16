9c1676fc60ed052e688250fec05451ea806a28d8
calmflow <o.newwday@gmail.com>
2024-04-16 23:59:30 +0900

    modify: service-list animation type

--- web2.1/js/shopping.js
+++ web2.1/js/shopping.js
@@ -1,23 +1,13 @@
 $(function () {
-    let clickNum = 0;
-
     //append,prepend를 이용한 슬라이드
 
-    // $('.btn-arrow--next').click(function () {
-    //     clickNum++;
+    $('.btn-arrow--next').click(function () {
+        let firstList = $('.service-list').eq(0);
+        let secondList = $('.service-list').eq(1);
 
-    //     if (clickNum > 2) {
-    //         clickNum = 1;
-    //     }
+        secondList.addClass('right-in');
+        firstList.removeClass('right-in');
 
-    //     console.log(clickNum);
-
-    //     let firstList = $('.service-list').eq(0);
-    //     let secondList = $('.service-list').eq(1);
-
-    //     secondList.addClass('right-in');
-    //     firstList.removeClass('right-in');
-
-    //     $('.service-list-wrap').append(firstList);
-    // });
+        $('.service-list-wrap').append(firstList);
+    });
 });
